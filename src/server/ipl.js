919f9573a807c3b20c44ed49cc549387d7266017
shubhamborke7778 <shubham.borke.17@mountblue.tech>
2021-08-21 16:29:13 +0530

    Add maches-Won-Per-Team

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -16,3 +16,26 @@ const matchesPlayedPerYear = function matchesPlayed(result) {
 console.log(matchesPlayedPerYear(matches));
 
 
+const matchWonPerTeam = function wonPerTeam(result) {
+    let matchWon = {};
+    for (let i = 0; i < result.length; i++) {
+        if (!matchWon.hasOwnProperty(result[i].winner) && result[i].winner !== "") {
+            matchWon[result[i].winner] = {};
+        }
+    }
+    for(let i = 0 ; i < result.length; i++){
+        for(let key in matchWon){
+            if(key === result[i].winner){
+                if(matchWon[result[i].winner][result[i].season]){
+                    matchWon[result[i].winner][result[i].season] += 1;
+                }else{
+                    matchWon[result[i].winner][result[i].season] = 1;
+                }
+            }
+        }
+            
+    }
+    return matchWon;
+}
+
+console.log(matchWonPerTeam(matches))
