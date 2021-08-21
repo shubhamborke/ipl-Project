8be6de5e4e2cf6905837d21b6b75d90a241e2a81
shubhamborke7778 <shubham.borke.17@mountblue.tech>
2021-08-21 16:26:25 +0530

    Add matches-Played-Per-Year

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,3 +1,18 @@
 import { matches } from "./csv-json.js";
 
-console.log(matches);
+
+const matchesPlayedPerYear = function matchesPlayed(result) {
+    let matchesPerYear = {};
+    for (let i = 0; i < result.length; i++) {
+        if (matchesPerYear.hasOwnProperty(result[i].season)) {
+            matchesPerYear[result[i].season] += 1;
+        } else {
+            matchesPerYear[result[i].season] = 1;
+        }
+    }
+    return matchesPerYear;
+}
+
+console.log(matchesPlayedPerYear(matches));
+
+
