1d381dd390b5a4e6923664013df7e836f8edbb48
shubhamborke7778 <shubham.borke.17@mountblue.tech>
2021-08-25 17:54:32 +0530

    Import extraRun function

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,15 @@
 import { matches } from './matchesData.js';
 import { deliveries } from './deliveriesData.js';
 // import { matchesPlayedPerYear } from './ipl.js';
-import { matchWonPerTeam } from './ipl.js';
-// import { extraRunsIn2016 } from './ipl.js';
+// import { matchWonPerTeam } from './ipl.js';
+import { extraRunsIn2016 } from './ipl.js';
 // import { topEconomicBowler } from './ipl.js';
 import fs from 'fs';
 
 
 // const matchesPerYear = matchesPlayedPerYear(matches);
-const matchesWonByTeam = matchWonPerTeam(matches);
-// const extraRun = extraRunsIn2016(deliveries);
+// const matchesWonByTeam = matchWonPerTeam(matches);
+const extraRun = extraRunsIn2016(deliveries);
 // const top10EconomiBowler = topEconomicBowler(deliveries);
 
 
@@ -22,21 +22,21 @@ const matchesWonByTeam = matchWonPerTeam(matches);
 //     }
 // });
 
-fs.writeFile('src/public/output/matchesWonPerYear.json', JSON.stringify(matchesWonByTeam,null,2), 'utf8', (err) =>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log("written successful");
-    }
-});
-
-// fs.writeFile('src/public/output/extraRunIn2016.json', JSON.stringify(extraRun,null,2), 'utf8', (err) =>{
+// fs.writeFile('src/public/output/matchesWonPerYear.json', JSON.stringify(matchesWonByTeam,null,2), 'utf8', (err) =>{
 //     if(err){
 //         console.log(err);
 //     }else{
 //         console.log("written successful");
 //     }
 // });
+
+fs.writeFile('src/public/output/extraRunIn2016.json', JSON.stringify(extraRun,null,2), 'utf8', (err) =>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("written successful");
+    }
+});
 
 // fs.writeFile('src/public/output/top10EconomiBowler.json', JSON.stringify(top10EconomiBowler,null,2), 'utf8', (err) =>{
 //     if(err){
