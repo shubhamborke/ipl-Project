e578d23b4f2e1e80620bef0cb138922f15b4e1a0
shubhamborke7778 <shubham.borke.17@mountblue.tech>
2021-08-30 17:40:58 +0530

    Add function for tossAndMatchWinner

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,17 +1,14 @@
-
 // matches played per year
 
 const matchesPlayedPerYear = function matchesPlayed(result) {
     let matchesPerYear = {};
     result.map(matchSeason => matchesPerYear.hasOwnProperty(matchSeason.season) ? matchesPerYear[matchSeason.season]
      += 1 : matchesPerYear[matchSeason.season] = 1);
-   
+
     return matchesPerYear;
 }
 
-
  export { matchesPlayedPerYear }
-
 
 // matches played by team per year
 
@@ -20,30 +17,24 @@ const matchWonPerTeam = function wonPerTeam(result) {
     result.map(match => matchWon[match.winner] = {});
     result.map(match => matchWon[match.winner][match.season] = 0);
     result.map(match => matchWon[match.winner][match.season] += 1);
-    
+
     return matchWon;
 }
 
-
 export { matchWonPerTeam }
-
 
 // extra runs conceded by team in 2016
 
 const extraRunsIn2016 = function extraRuns(result) {
     let extraRun = {};
     result.filter(matche => matche.match_id >= 577).map(bowl => extraRun[bowl.bowling_team] ? extraRun[bowl.bowling_team] += Number(bowl.extra_runs) : extraRun[bowl.bowling_team] = Number(bowl.extra_runs))
-   
+
     return extraRun;
 }
 
-
 export { extraRunsIn2016 }
 
-
-
 // // Economical top 10 bowler
-
 
 const topEconomicBowler = function economicBowlers(result) {
     let totalOver = {};
@@ -74,5 +65,23 @@ const topEconomicBowler = function economicBowlers(result) {
     return economyPlayers;
 }
 
-
 export { topEconomicBowler }
+
+// Won tosses and matches
+
+const tossAndMatchWon = function tossAndMachesWinner(result) {
+  let tossMatch = {};
+  result
+    .filter((tossAndMatch) => tossAndMatch.toss_winner === tossAndMatch.winner)
+    .map((tossWin) =>
+      tossMatch[tossWin.winner]
+        ? (tossMatch[tossWin.winner] += 1)
+        : (tossMatch[tossWin.winner] = 1)
+    );
+  return tossMatch;
+};
+
+export { tossAndMatchWon }
+
+
+
