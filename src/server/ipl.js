fa46821d2e80f8854340ced5d8f2df5ec6188b17
shubhamborke7778 <shubham.borke.17@mountblue.tech>
2021-09-03 16:07:36 +0530

    Modify code to reduce complexity

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,57 +1,107 @@
-
 // matches played per year
+
+
 
 const matchesPlayedPerYear = function matchesPlayed(result) {
     let matchesPerYear = {};
     result.map(matchSeason => matchesPerYear.hasOwnProperty(matchSeason.season) ? matchesPerYear[matchSeason.season]
      += 1 : matchesPerYear[matchSeason.season] = 1);
-   
+
     return matchesPerYear;
 }
 
-
  export { matchesPlayedPerYear }
-
 
 // matches played by team per year
 
+
 const matchWonPerTeam = function wonPerTeam(result) {
-    let matchWon = {};
+  let matchWon = {};
+  result.map((match) =>
+    matchWon[match.winner]
+      ? matchWon[match.winner][match.season]
+        ? (matchWon[match.winner][match.season] += 1)
+        : (matchWon[match.winner][match.season] = 1)
+      : (matchWon[match.winner] = {})
+  );
+/*  
+            code with if-else seperate
+    
+      if(matchWon[match.winner]){
+      if(matchWon[match.winner][match.season]){
+        matchWon[match.winner][match.season] += 1;
+      }else{
+        matchWon[match.winner][match.season]= 1;
+      }
+    }else{
+      matchWon[match.winner] = {}
+    }
+      
+    })
+
+    */
+/*   code with more iteration seperate
+
+
     result.map(match => matchWon[match.winner] = {});
     result.map(match => matchWon[match.winner][match.season] = 0);
     result.map(match => matchWon[match.winner][match.season] += 1);
-    
-    return matchWon;
-}
+    */
+  return matchWon;
+};
 
-
-export { matchWonPerTeam }
-
+export { matchWonPerTeam };
 
 // extra runs conceded by team in 2016
 
-const extraRunsIn2016 = function extraRuns(result) {
-    let extraRun = {};
-    result.filter(matche => matche.match_id >= 577).map(bowl => extraRun[bowl.bowling_team] ? extraRun[bowl.bowling_team] += Number(bowl.extra_runs) : extraRun[bowl.bowling_team] = Number(bowl.extra_runs))
-   
-    return extraRun;
-}
+const extraRunsIn2016 = function extraRuns(matchData, deliveryData) {
+  let extraRun = {};
+
+  matchData
+    .filter((season) => season.season === "2016")
+    .map((ids) => {
+      let matchId = ids.id;
+      return deliveryData
+        .filter((compareId) => compareId.match_id === matchId)
+        .map((teamExist) =>
+          extraRun[teamExist.bowling_team]
+            ? (extraRun[teamExist.bowling_team] += Number(teamExist.extra_runs))
+            : (extraRun[teamExist.bowling_team] = Number(teamExist.extra_runs))
+        );
+    });
+
+//   /*  direct id method will have biggest disadvantage
+//     result.filter(match => match.match_id >= 577).map(bowl => extraRun[bowl.bowling_team] ? extraRun[bowl.bowling_team] += Number(bowl.extra_runs) : extraRun[bowl.bowling_team] = Number(bowl.extra_runs)) */
+
+  return extraRun;
+};
+
+export { extraRunsIn2016 };
 
 
-export { extraRunsIn2016 }
+// // // Economical top 10 bowler
 
 
-
-// // Economical top 10 bowler
-
-
-const topEconomicBowler = function economicBowlers(result) {
+const topEconomicBowler = function economicBowlers(matchData, deliveryData) {
     let totalOver = {};
     let totalRuns = {};
     let economic = {};
     let economyPlayers = {};
-    result.filter(delivery => delivery.match_id >= 518 && delivery.match_id <= 576).map(runs => totalRuns[runs.bowler] ? totalRuns[runs.bowler] += +runs.wide_runs + +runs.bye_runs + +runs.legbye_runs + +runs.noball_runs + +runs.penalty_runs + +runs.batsman_runs + +runs.extra_runs: totalRuns[runs.bowler] = +runs.wide_runs + +runs.bye_runs + +runs.legbye_runs + +runs.noball_runs + +runs.penalty_runs + +runs.batsman_runs + +runs.extra_runs);
-    result.filter(delivery => delivery.match_id >= 518 && delivery.match_id <= 576).map(overs => totalOver[overs.bowler] ? totalOver[overs.bowler] += 1 : totalOver[overs.bowler] = 1);
+    matchData.filter(season => season.season === "2015").map(ids => {
+      let matchId = ids.id;
+     deliveryData.filter(compareId => compareId.match_id === matchId).map(teamExist => {
+        if(totalRuns[teamExist.bowler]){
+          totalRuns[teamExist.bowler] += Number(teamExist.total_runs);
+        }else{
+          totalRuns[teamExist.bowler] = Number(teamExist.total_runs);
+        }
+        if(totalOver[teamExist.bowler]){
+          totalOver[teamExist.bowler] += 1;
+        }else{
+          totalOver[teamExist.bowler] = 1;
+        }
+      })
+    })
     for (let bowler in totalOver) {
         totalOver[bowler] /= 6;
     }
@@ -71,6 +121,12 @@ const topEconomicBowler = function economicBowlers(result) {
             }
         }
     });
+
+    /*    Avoided itrating over and over on data. program will blocked or complexity occuers
+    // result.filter(delivery => delivery.match_id >= 518 && delivery.match_id <= 576).map(runs => totalRuns[runs.bowler] ? totalRuns[runs.bowler] += +runs.wide_runs + +runs.bye_runs + +runs.legbye_runs + +runs.noball_runs + +runs.penalty_runs + +runs.batsman_runs + +runs.extra_runs: totalRuns[runs.bowler] = +runs.wide_runs + +runs.bye_runs + +runs.legbye_runs + +runs.noball_runs + +runs.penalty_runs + +runs.batsman_runs + +runs.extra_runs);
+    // result.filter(delivery => delivery.match_id >= 518 && delivery.match_id <= 576).map(overs => totalOver[overs.bowler] ? totalOver[overs.bowler] += 1 : totalOver[overs.bowler] = 1);
+    
+    */
     return economyPlayers;
 }
 
